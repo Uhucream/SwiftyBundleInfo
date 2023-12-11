094ba514a633b4e82b4474da467a780a19d432da
Uhucream <aborog1324@yahoo.co.jp>
2023-12-11 23:56:48 +0900

    feat: created extension for computed property of `Bundle.infoDictionary`

---
+++ Sources/SwiftyBundleInfo/Extensions/Foundation/Bundle+.swift
@@ -0,0 +1,26 @@
+//
+//  Bundle+.swift
+//
+//
+//  Created by Takashi Ushikoshi on 2023/11/25.
+//
+
+import DictionaryCoder
+import Foundation
+
+public extension Bundle {
+    typealias Info = BundleInfo
+}
+
+public extension Bundle {
+    /// A decoded struct containing bundle information
+    var info: Info? {
+        get {
+            guard let infoDictionary else { return nil }
+
+            let dictionaryDecoder: DictionaryDecoder = .default
+
+            return try? dictionaryDecoder.decode(BundleInfo.self, from: infoDictionary)
+        }
+    }
+}
